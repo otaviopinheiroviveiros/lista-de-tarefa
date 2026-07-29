6c9b57f09c9080b7dd173718f758968b0c01e274
otaviopinherio <otaviopinheiroviveiro09@gmail.com>
2026-07-29 19:02:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lista de tarefa-site
+Meu primeiro CRUD de tarefas utilizando HTML, CSS e JavaScript.
---
+++ script.js
@@ -0,0 +1,17 @@
+const butaoaddTarefa = document.querySelector("#butao-adicionar-tarefa")
+const butaoaVERTarefa = document.querySelector("#butao-verTarefa")
+butaoaVERTarefa.addEventListener("click",verTarefa)
+butaoaddTarefa.addEventListener("click",addTarefa)
+
+let conteinerInicio = document.querySelector(".conteiner")
+let conteinerAddTarefa = document.querySelector(".conteiner-adicionar-tarefa")
+let conteiner3janela = document.querySelector(".conteiner-3janela")
+
+function addTarefa(){
+    
+}
+
+
+function verTarefa(){
+
+}
